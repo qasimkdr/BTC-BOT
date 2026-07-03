7c874b4261d086a1f5c961421399b735e725418b
qasimkdr <kdrqasim@gmail.com>
2026-07-03 21:20:36 +0500

    Update PriceCard.jsx

--- frontend/src/components/PriceCard.jsx
+++ frontend/src/components/PriceCard.jsx
@@ -1,4 +1,8 @@
-import { useEffect, useState } from "react";
+import {
+  useEffect,
+  useState,
+} from "react";
+
 import socket from "../services/socket";
 
 const PriceCard = () => {
@@ -6,24 +10,73 @@ const PriceCard = () => {
     useState(0);
 
   useEffect(() => {
-    socket.on(
-      "price-update",
+
+    const handleConnect =
+      () => {
+        console.log(
+          "✅ Socket Connected:",
+          socket.id
+        );
+      };
+
+    const handleDisconnect =
+      () => {
+        console.log(
+          "❌ Socket Disconnected"
+        );
+      };
+
+    const handlePrice =
       (data) => {
+        console.log(
+          "📈 Price Update:",
+          data
+        );
+
         setPrice(
           data.price
         );
-      }
+      };
+
+    socket.on(
+      "connect",
+      handleConnect
+    );
+
+    socket.on(
+      "disconnect",
+      handleDisconnect
+    );
+
+    socket.on(
+      "price-update",
+      handlePrice
     );
 
     return () => {
+
       socket.off(
-        "price-update"
+        "connect",
+        handleConnect
       );
+
+      socket.off(
+        "disconnect",
+        handleDisconnect
+      );
+
+      socket.off(
+        "price-update",
+        handlePrice
+      );
+
     };
+
   }, []);
 
   return (
     <div className="bg-zinc-900 p-5 rounded-xl border border-zinc-800">
+
       <h2 className="text-xl font-semibold">
         BTCUSDT Price
       </h2>
@@ -31,6 +84,7 @@ const PriceCard = () => {
       <p className="text-4xl font-bold mt-4 text-green-400">
         ${price}
       </p>
+
     </div>
   );
 };
