5c963abe19ee211067be5b761b488ca39985e674
qasimkdr <kdrqasim@gmail.com>
2026-07-04 10:54:11 +0500

    Change grid layout from 3 to 2 columns

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -15,7 +15,7 @@ const Dashboard = () => {
 
       
       {/* Other Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <PriceCard />
         
         <StatsCard />
