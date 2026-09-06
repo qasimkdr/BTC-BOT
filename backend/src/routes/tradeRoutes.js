c22efcfd96b79cde6af6c292698ee48eaf9a1d9a
qasimkdr <kdrqasim@gmail.com>
2026-09-06 17:55:46 +0500

    Expose full trade export route

--- backend/src/routes/tradeRoutes.js
+++ backend/src/routes/tradeRoutes.js
@@ -3,19 +3,13 @@ import express from "express";
 import {
   getTrades,
   getActiveTrade,
+  exportTrades,
 } from "../controllers/tradeController.js";
 
-const router =
-  express.Router();
+const router = express.Router();
 
-router.get(
-  "/",
-  getTrades
-);
-
-router.get(
-  "/active",
-  getActiveTrade
-);
+router.get("/", getTrades);
+router.get("/active", getActiveTrade);
+router.get("/export", exportTrades);
 
 export default router;
