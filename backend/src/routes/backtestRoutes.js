8e7ced7183f801b67216e88cc6ff8dc932de5435
qasimkdr <kdrqasim@gmail.com>
2026-09-06 19:37:15 +0500

    Keep only V2 backtest route

--- backend/src/routes/backtestRoutes.js
+++ backend/src/routes/backtestRoutes.js
@@ -1,19 +1,8 @@
 import express from "express";
-
-import {
-  runBacktest,
-} from "../controllers/backtestController.js";
-import {
-  runBacktestV1,
-} from "../controllers/backtestV1Controller.js";
-import {
-  runBacktestV3,
-} from "../controllers/backtestV3Controller.js";
+import { runBacktest } from "../controllers/backtestController.js";
 
 const router = express.Router();
 
 router.get("/", runBacktest);
-router.get("/v1", runBacktestV1);
-router.get("/v3", runBacktestV3);
 
 export default router;
