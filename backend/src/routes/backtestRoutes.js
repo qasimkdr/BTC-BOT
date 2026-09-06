53959dece40ba96790a0b607a84c786cf257c43c
qasimkdr <kdrqasim@gmail.com>
2026-09-06 21:19:21 +0500

    Expose V2 walk-forward robustness endpoint

--- backend/src/routes/backtestRoutes.js
+++ backend/src/routes/backtestRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { runBacktest } from "../controllers/backtestController.js";
 import { runBacktestValidation } from "../controllers/backtestValidationController.js";
+import { runWalkForward } from "../controllers/walkForwardController.js";
 
 const router = express.Router();
 
 router.get("/", runBacktest);
 router.get("/validate", runBacktestValidation);
+router.get("/walk-forward", runWalkForward);
 
 export default router;
